49d55aafd46986484464f5136e71c303c9f93f30
hfnu_2010211112 <2942885033@qq.com>
2024-03-07 23:51:47 +0800

    init commit

---
+++ cloud_provider_payment8001/src/main/java/com/at/cloud/CloudProviderPaymentApplication8001.java
@@ -0,0 +1,16 @@
+package com.at.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Husp
+ * @Date 2024/3/7 23:24
+ */
+@SpringBootApplication
+public class CloudProviderPaymentApplication8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudProviderPaymentApplication8001.class, args);
+    }
+}
---
+++ cloud_provider_payment8001/src/test/java/com/at/cloud/CloudProviderPaymentTest.java
@@ -0,0 +1,11 @@
+package com.at.cloud;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author Husp
+ * @Date 2024/3/7 23:47
+ */
+@SpringBootTest
+public class CloudProviderPaymentTest {
+}

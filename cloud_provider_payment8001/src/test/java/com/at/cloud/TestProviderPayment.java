9a4890cd670cb846dc894d6ba9e45d26ebe7862b
hfnu_2010211112 <2942885033@qq.com>
2024-03-14 22:51:50 +0800

    test: 支付CRUD单元测试驱动开发

--- cloud_provider_payment8001/src/test/java/com/at/cloud/TestProviderPayment.java
+++ cloud_provider_payment8001/src/test/java/com/at/cloud/TestProviderPayment.java
@@ -1,12 +1,14 @@
 package com.at.cloud;
 
 import com.at.cloud.entities.Pay;
+import com.at.cloud.entities.dto.PayDTO;
 import com.at.cloud.service.IPayService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -20,10 +22,41 @@ public class TestProviderPayment {
     private IPayService payService;
 
     @Test
-    @DisplayName("查询所有支付信息")
+    @DisplayName(value = "测试查询所有支付信息")
     public void testGetAll() {
         List<Pay> payList = payService.getAll();
         System.out.println("payList = " + payList);
+    }
+
+    @Test
+    @DisplayName(value = "测试新增支付")
+    public void testAddPay(){
+        Pay pay = new Pay();
+        pay.setPayNo("No.202403141001");
+        pay.setOrderNo("No.202403142001");
+        pay.setAmount(BigDecimal.valueOf(29.9));
+        int row = payService.addPay(pay);
+        System.out.println("row = " + row);
+    }
+
+    @Test
+    @DisplayName(value = "测试删除支付")
+    public void testDeletePay(){
+        Long id = 12L;
+        Integer row = payService.deletePay(id);
+        System.out.println("row = " + row);
+    }
+
+    @Test
+    @DisplayName(value = "测试修改支付")
+    public void testUpdatePay(){
+        Pay pay = new Pay();
+        pay.setId(11L);
+        pay.setPayNo("pay202403131001");
+        pay.setOrderNo("pay202403132001");
+        pay.setAmount(BigDecimal.valueOf(49.9));
+        int row = payService.updatePay(pay);
+        System.out.println("row = " + row);
     }
 
 }

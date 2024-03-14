20e9304da63e2fd32b026f06f771f00dfbd42473
shunpeng.hu <shunpeng.hu@9fzt.com.cn>
2024-03-14 13:29:43 +0800

    fix: 修改变量

--- cloud_consumer_order8002/src/main/java/com/at/cloud/constant/SystemConstant.java
+++ cloud_consumer_order8002/src/main/java/com/at/cloud/constant/SystemConstant.java
@@ -4,7 +4,7 @@ package com.at.cloud.constant;
  * @Author Husp
  * @Date 2024/3/12 22:18
  */
-public class ServiceConstant {
+public class SystemConstant {
 
     public static final String PAYMENT_BASE_URL = "http://localhost:8001";
 }
--- cloud_consumer_order8002/src/main/java/com/at/cloud/controller/OrderController.java
+++ cloud_consumer_order8002/src/main/java/com/at/cloud/controller/OrderController.java
@@ -1,7 +1,7 @@
 package com.at.cloud.controller;
 
 import com.at.cloud.common.ResultData;
-import com.at.cloud.constant.ServiceConstant;
+import com.at.cloud.constant.SystemConstant;
 import com.at.cloud.entities.dto.PayDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -25,26 +25,26 @@ public class OrderController {
     @Operation(tags = "新增", summary = "新增", description = "服务消费新增支付")
     @GetMapping("/add")
     public ResultData addPay(@RequestBody @Parameter PayDTO payDTO) {
-        return restTemplate.postForObject(ServiceConstant.PAYMENT_BASE_URL + "/provider/pay/add", payDTO, ResultData.class);
+        return restTemplate.postForObject(SystemConstant.PAYMENT_BASE_URL + "/provider/pay/add", payDTO, ResultData.class);
     }
 
     @Operation(tags = "查询单个", summary = "查询单个", description = "服务消费查询单个支付")
     @GetMapping("/selectOne/{id}")
     public ResultData getPayById(@PathVariable("id") @Parameter Long id) {
-        return restTemplate.getForObject(ServiceConstant.PAYMENT_BASE_URL + "/provider/pay/selectOne/" + id, ResultData.class, id);
+        return restTemplate.getForObject(SystemConstant.PAYMENT_BASE_URL + "/provider/pay/selectOne/" + id, ResultData.class, id);
     }
 
     @Operation(tags = "修改", summary = "修改", description = "服务消费修改支付")
     @GetMapping("/update")
     public ResultData updatePay(@RequestBody @Parameter PayDTO payDTO) {
-        restTemplate.put(ServiceConstant.PAYMENT_BASE_URL + "/provider/pay/update", payDTO);
+        restTemplate.put(SystemConstant.PAYMENT_BASE_URL + "/provider/pay/update", payDTO);
         return ResultData.success("修改成功");
     }
 
     @Operation(tags = "删除", summary = "删除", description = "服务消费删除支付")
     @GetMapping("/delete/{id}")
     public ResultData delete(@PathVariable("id") @Parameter Long id){
-        restTemplate.delete(ServiceConstant.PAYMENT_BASE_URL + "/provider/pay/delete/"+id, id);
+        restTemplate.delete(SystemConstant.PAYMENT_BASE_URL + "/provider/pay/delete/"+id, id);
         return ResultData.success("删除成功");
     }
 

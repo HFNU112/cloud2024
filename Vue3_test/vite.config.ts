0f9fc6037c41a85a4209246b8a70efc664c78956
hfnu_2010211112 <2942885033@qq.com>
2024-06-06 01:03:18 +0800

    vue3的setup语法糖配置插件

--- Vue3_test/vite.config.ts
+++ Vue3_test/vite.config.ts
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // vue3.2 setup语法糖省略指定组件名script标签
+    VueSetupExtend()
   ],
   resolve: {
     alias: {

f681186bf00a6a52b87b27ccb80b5492d8651990
shunpeng.hu <shunpeng.hu@9fzt.com.cn>
2024-06-14 15:03:09 +0800

    feat(nacos-config): nacos配置客户端

---
+++ cloudalibaba_config_nacos_client3366/src/main/java/com/at/cloud/CloudAlibabaConfigNacosClientApplication3366.java
@@ -0,0 +1,17 @@
+package com.at.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author shunpeng.hu
+ * @date 2024/6/14 13:55
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CloudAlibabaConfigNacosClientApplication3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAlibabaConfigNacosClientApplication3366.class, args);
+    }
+}

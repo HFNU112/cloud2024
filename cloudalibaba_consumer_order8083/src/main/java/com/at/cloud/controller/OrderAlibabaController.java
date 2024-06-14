601e142f20570729a3945aa1015915a96efe90e2
shunpeng.hu <shunpeng.hu@9fzt.com.cn>
2024-06-14 11:08:26 +0800

    feat(nacos): 初始化Nacos消费者模块

---
+++ cloudalibaba_consumer_order8083/src/main/java/com/at/cloud/controller/OrderAlibabaController.java
@@ -0,0 +1,29 @@
+package com.at.cloud.controller;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author shunpeng.hu
+ * @date 2024/6/14 10:36
+ */
+@RestController
+public class OrderAlibabaController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverURL;
+
+    @GetMapping("/consumer/pay/nacos/{id}")
+    public String checkPaymentInfo(@PathVariable("id") Integer id)
+    {
+        String result = restTemplate.getForObject(serverURL + "/pay/nacos/" + id, String.class);
+        return result+"\t"+"    我是OrderNacosController8083调用者***********";
+    }
+}
